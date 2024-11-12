0f742ec8d3112fc0fa1aff9715040e6b90f80b17
Joyoounjin <dbswls060@gmail.com>
2024-11-12 23:52:08 +0900

    Feat : 플레이어 컨디션 기능

--- Survival-Dungeon/Assets/Scripts/Player.cs
+++ Survival-Dungeon/Assets/Scripts/Player.cs
@@ -5,10 +5,12 @@ public class Player : MonoBehaviour
     // 플레이어 데이터 관리 
 
     public PlayerController controller;
+    public PlayerCondition condition;
 
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
     }
 }
---
+++ Survival-Dungeon/Assets/Scripts/UICondition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UICondition : MonoBehaviour
+{
+    // Condition 클래스의 속성과 메서드를 모두 가진 맴버 변수로 생성
+    // 필드에서 선언만 하는 이유는 인스펙터로 연결해서 쓰기 위함이고, UI 클래스에서 속성과 메서드를 쓰지 않기 때문 
+    // 현 상태 : 참조 관계는 형성되어 접근은 가능하지만, 초기화(생성) 되지 않아 해당 클래스에서 호출은 불가능함
+    // 만약 게임 오브젝트로 선언하고 인스펙터로 연결한다면 참조 관계가 형성되지 않음 
+
+    public Condition health;
+    public Condition hunger;
+    public Condition stamina;
+
+    private void Start()
+    {
+        CharacterManager.Instance.Player.condition.uiCondition = this;
+    }
+}
