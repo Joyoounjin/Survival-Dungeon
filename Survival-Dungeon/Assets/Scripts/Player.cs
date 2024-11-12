fbe600d1b9de353ae205de1d6ebb51cec4318d8c
Joyoounjin <dbswls060@gmail.com>
2024-11-12 19:27:09 +0900

    Feat : 플레이어 기본 세팅
    - 플레이어 싱글톤 작업
    - 플레이어 인풋시스템 작업
    - 베이직반 Q1 작업

---
+++ Survival-Dungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // 플레이어 데이터 관리 
+
+    public PlayerController controller;
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+    }
+}
